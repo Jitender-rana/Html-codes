f7a69534693c6ea79d076bed8f0bd232ef4da315
Jitender-rana <119347955+Jitender-rana@users.noreply.github.com>
2024-03-13 21:40:07 +0530

    Added DOM+Challenge+Starting+Files

---
+++ DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -0,0 +1,4 @@
+var list=document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild;
+list.innerHTML="JItender Rana";
+document.querySelector("a").style.color="red";
+document.querySelector("h1").classList.toggle("huge");
